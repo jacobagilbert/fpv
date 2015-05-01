ed920b1ddce56d61b6cd9ce274bc1c2d5af34e23
Jacob <mrjacobagilbert@gmail.com>
2015-05-01 04:58:23 -0700

    updated attiny firmware for more correct SPI operation and better defined button interface

--- ZMR250_FPV_TX/attiny/attiny84_transmitter.ino
+++ ZMR250_FPV_TX/attiny/attiny84_transmitter.ino
@@ -1,34 +1,70 @@
-// code for transmitter
+/*
+ *  BOSCAM TX5823-based 5.8Ghz 200mW FPV transmitter
+ *
+ *  Author: J. A. Gilbert
+ *  Date:   April 2015
+ *  Info:   https://github.com/mrjacobagilbert/fpv
+ *  
+ *  Description:
+ *    This code is designed for the ATTiny44/84, with optimization it may run on a 24. It
+ *    will display the current band/channel on startup, set the frequency and wait for
+ *    user input. The operator can change frequency with a long button press and a quick
+ *    release after the LED turns on.
+ *
+ *
+ *  Programming Information:
+ *    Using ArduinoISP sketch, connect the following pins. 3.3k resistors should be used
+ *    if the output is higher voltage than the input (eg: on RST/SCK/MOSI if Arduino is +5V
+ *    and ATTiny is +3.3v, MISO does not need one).
+ *
+ *       ARDUINO              ATTINY
+ *         D10 ----------->> RST  (4)
+ *         D11 ----------->> MOSI (7)
+ *         D12 <<----------- MISO (8)
+ *         D13 ----------->> SCK  (9)
+ *         GND ------------- GND (14)
+ *
+ *  This sketch is designed to run with an 8MHz oscillator. Burn appropriate bootloader.
+ */
+ 
 #include <EEPROM.h>
 
-static const uint8_t SPI_ENABLE_PIN = 9;
-static const uint8_t SPI_CLOCK_PIN = 8;
-static const uint8_t SPI_DATA_PIN = 10;
-static const uint8_t SWITCH_PIN = 2;
-static const uint8_t LED_PIN = 7;
-static const uint8_t REMOTE_PIN = 0;
+static const uint8_t SPI_ENABLE_PIN = 9;        // ATTiny Pin 3
+static const uint8_t SPI_CLOCK_PIN = 8;         // ATTiny Pin 5
+static const uint8_t SPI_DATA_PIN = 10;         // ATTiny Pin 2
+static const uint8_t SWITCH_PIN = 2;            // ATTiny Pin 11
+static const uint8_t LED_PIN = 7;               // ATTiny Pin 6
+static const uint8_t REMOTE_PIN = 0;            // ATTiny Pin 13
 
-static const uint8_t EEPROM_ADDR_LAST_FREQ = 0;
+static const uint8_t EEPROM_ADDR_LAST_FREQ = 1;
 static const uint8_t NUM_BANDS = 5;
 
-/* These frequencies are for RichWave transmitters - this table is the lower 16 bits - upper byte is 0x04 */
-const uint16_t channelTable[] PROGMEM = {
-  0x7981,    0x758D,    0x7199,    0x6DA5,    0x69B1,    0x65BD,    0x6209,    0x5E15,    // Band A
-  0x5F9D,    0x6338,    0x6713,    0x6AAE,    0x6E89,    0x7224,    0x75BF,    0x799A,    // Band B
-  0x5A21,    0x562D,    0x5239,    0x4E85,    0x7D35,    0x8129,    0x851D,    0x8911,    // Band E
-  0x610C,    0x6500,    0x68B4,    0x6CA8,    0x709C,    0x7490,    0x7884,    0x7C38,    // Band F / Airwave
-  0x510A,    0x5827,    0x5F84,    0x66A1,    0x6DBE,    0x751B,    0x7C38,    0x8395     // Band R / IRC Raceband
+/* These frequencies are for RichWave transmitters - this table is the lower 16 bits - upper 4 bits are 0x4 */
+static const uint16_t channelTable[] PROGMEM = {
+//   CH 1       CH 2       CH 3       CH 4       CH 5       CH 6       CH 7       CH 8
+    0x7981,    0x758D,    0x7199,    0x6DA5,    0x69B1,    0x65BD,    0x6209,    0x5E15,    // Band A
+    0x5F9D,    0x6338,    0x6713,    0x6AAE,    0x6E89,    0x7224,    0x75BF,    0x799A,    // Band B
+    0x5A21,    0x562D,    0x5239,    0x4E85,    0x7D35,    0x8129,    0x851D,    0x8911,    // Band E
+    0x610C,    0x6500,    0x68B4,    0x6CA8,    0x709C,    0x7490,    0x7884,    0x7C38,    // Band F / Airwave
+    0x510A,    0x5827,    0x5F84,    0x66A1,    0x6DBE,    0x751B,    0x7C38,    0x8395     // Band R / Raceband
 };
+const uint16_t rCounter = 0x0190;    // default value, provides 40kHz frequency resolution
 
-enum STATE { WAITING, HOLDOFF, SHOW_FREQ, RELEASE_TIMER, BAND_COUNTING, CHANNEL_SET, CHANNEL_COUNTING, SET_FREQ };
-STATE current_state;
-uint8_t current_freq;
+/* Frequency mapping for channelTable (values in MHz):
+ *   CH 1       CH 2       CH 3       CH 4       CH 5       CH 6       CH 7       CH 8
+ *   5865       5845       5825       5805       5785       5765       5745       5725      // Band A
+ *   5733       5752       5771       5790       5809       5828       5847       5866      // Band B
+ *   5705       5685       5665       5645       5885       5905       5925       5945      // Band E
+ *   5740       5760       5780       5800       5820       5840       5860       5880      // Band F / Airwave
+ *   5658       5695       5732       5769       5806       5843       5880       5917      // Band R / RaceBand
+ */
 
+enum STATE { WAITING, HOLDOFF, SHOW_FREQ, RELEASE_TIMER, BAND_COUNTING, CHANNEL_COUNTING, SET_FREQ } current_state;
 uint16_t count;
-uint8_t previous_val, band, channel;
+uint8_t current_freq, previous_val, band, channel;
 
 void setup() {
- 
+  
   // set pin directions
   pinMode(SPI_ENABLE_PIN, OUTPUT);
   pinMode(SPI_CLOCK_PIN, OUTPUT);
@@ -37,35 +73,42 @@ void setup() {
   pinMode(LED_PIN, OUTPUT);
   pinMode(REMOTE_PIN, INPUT_PULLUP);
   
+  // initialize SPI bus
   digitalWrite(SPI_CLOCK_PIN, LOW);
-  digitalWrite(SPI_ENABLE_PIN, LOW);
+  digitalWrite(SPI_ENABLE_PIN, HIGH);
+  
+  // LED is OFF by default
+  digitalWrite(LED_PIN, LOW);
   
   // give transmitter time to power up
   delay(1000);
   
+  // set the R counter-divider which does not change
+  spi_write(0x00, rCounter);                                 // write R counter-divider (fixed value)
   // read previous channel and set module
   current_freq = EEPROM.read(EEPROM_ADDR_LAST_FREQ) & 0x3f;  // mask by 0b0011.1111 = 0b xx xx B2 B1 . B0 C2 C1 C0
   set_5823_freq(current_freq);
   
   //flash current frequency to user on boot
-  flash_freq(current_freq);
+  flashFreq(current_freq);
   
   current_state = WAITING;
 }
 
 void loop() {
   
-  // we don't need to do ANYTHING unless input changes
-
+  // main loop is just a switch statement
   switch (current_state) {
     
+//----------------------- WAITING FOR USER INPUT ----------------------------//
     case WAITING:
-      if (switch_read(10)) {
+      if (button_read(10)) {
         current_state = HOLDOFF;
         count = 0;
       }
       break;
-    
+      
+//----------------------- CHECKING DURATION OF BUTTON PRESS -----------------//
     // must hold button for two seconds
     case HOLDOFF:
       if (count > 200) {
@@ -73,7 +116,7 @@ void loop() {
         count = 0;
         current_state = RELEASE_TIMER;
       } else {
-        if (!switch_read(10)) {
+        if (!button_read(10)) {
           // if just a quick press, flash the frequency
           if (count < 25) {
             current_state = SHOW_FREQ;
@@ -86,19 +129,21 @@ void loop() {
       }
       count++;
       break;
-    
+      
+//----------------------- DISPLAY FREQUENCY FOR SHORT PRESS -----------------//
     case SHOW_FREQ:
-      flash_freq(current_freq);
+      flashFreq(current_freq);
       current_state = WAITING;
       break;
-    
+      
+//----------------------- CHECKING FOR BUTTON RELEASE -----------------------//
     // must release button within 350 ms of LED going off
     case RELEASE_TIMER:
-      if (count > 35) {
-        digitalWrite(LED_PIN, LOW);
-        current_state = WAITING;
-      } else {
-        if (!switch_read(10)) {
+      if (!button_read(10)) {
+        if (count > 35) {
+          digitalWrite(LED_PIN, LOW);
+          current_state = WAITING;
+        } else {
           digitalWrite(LED_PIN, LOW);
           
           // indicate band-setting mode when released
@@ -109,12 +154,17 @@ void loop() {
           previous_val = 0;
           current_state = BAND_COUNTING;
         }
+      } else {
+        if (count > 30) {
+          digitalWrite(LED_PIN, LOW);
+        }
       }
       count++;
       break;
-    
+      
+//----------------------- USER BAND SELECTION -------------------------------//
     case BAND_COUNTING:
-      if (switch_read(10)) {
+      if (button_read(10)) {
         if (previous_val == 0) {
           blink_led(20,0,1);
           count = 0;    // reset the counter if button was pressed
@@ -143,26 +193,15 @@ void loop() {
           previous_val = 0;
           current_state = CHANNEL_COUNTING;
         } else {
-          blink_led(25, 25, 16);  // ERROR
+          flashError();  // flash error sequence
           current_state = WAITING;
         }
       }
       break;
-    
-    case CHANNEL_SET:
-      if (!switch_read(10)) {
-        // indicate band-setting mode when released
-        delay(100);
-        blink_led(75,25,5);
-        count = 0;
-        channel = 0;
-        current_state = CHANNEL_COUNTING;
-        previous_val = 0;
-      }
-      break;
-    
+      
+//----------------------- USER CHANNEL SELECTION ----------------------------//
     case CHANNEL_COUNTING:
-      if (switch_read(10)) {
+      if (button_read(10)) {
         if (previous_val == 0) {
           blink_led(20,0,1);
           count = 0;    // reset the counter if button was pressed
@@ -186,12 +225,13 @@ void loop() {
           delay(500);
           current_state = SET_FREQ;
         } else {
-          blink_led(25, 25, 16);  // ERROR
+          flashError();     // flash error sequence
           current_state = WAITING;
         }
       }
       break;
-    
+      
+//----------------------- SETTING FREQUENCY ---------------------------------//
     case SET_FREQ:
       current_freq = (band - 1) * 8 + (channel - 1);
       
@@ -202,21 +242,16 @@ void loop() {
       set_5823_freq(current_freq);
       
       // display the frequency to the user
-      delay(1000);
-      blink_led(500,0,1);
-      delay(1000);
-      flash_freq(current_freq);
-      delay(1000);
-      blink_led(500,0,1);
+      flashFreq(current_freq);
       
       current_state = WAITING;
       break;
       
+//----------------------- BY DEFAULT, RETURN TO WAITING ---------------------//
     default:
       current_state = WAITING;
       break;
   }
-
 }
 
 
@@ -224,8 +259,11 @@ void loop() {
 //////////////////////////////////     SUBROUTINES     //////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////
 
-byte switch_read(uint16_t time)
-// accepts configurable debounce time, favor unpressed ('1' due to internal pullup)
+/*
+ *  read the button applying a very simple software-debounce routine accepts
+ *  configurable debounce time, favor unpressed ('1' due to internal pullup)
+ */
+byte button_read(uint16_t time)
 {
   if (digitalRead(SWITCH_PIN) == 0) {
     delay(time);
@@ -239,24 +277,14 @@ byte switch_read(uint16_t time)
 }
 
 
-void flash_freq(uint8_t freq)
-{
-  uint8_t band, channel;
-  
-  band = (freq >> 3) + 1;         // band is the band/8 + 1
-  channel = (freq & 0x07) + 1;   // channel is lowest 3 bytes + 1
-  
-  blink_led(100,200,band);
-  delay(1000);
-  blink_led(100,200,channel);
-}
-
-
+/*
+ *  blink the LED with definable duty cycle and number of times
+ */
 void blink_led(uint16_t ms_on, uint16_t ms_off, uint16_t n_times)
 {
   uint16_t ii;
   
-  for (ii=0; ii<n_times; ii++) {
+  for (ii = n_times; ii > 0; ii--) {
     digitalWrite(LED_PIN, HIGH);
     delay(ms_on);
     digitalWrite(LED_PIN, LOW);
@@ -265,110 +293,81 @@ void blink_led(uint16_t ms_on, uint16_t ms_off, uint16_t n_times)
 }
 
 
+/*
+ *  show the current frequency by blinking the LED
+ */
+void flashFreq(uint8_t freq)
+{
+  //blink_led(750,0,1);                        // long blink
+  delay(750);                                // delay
+  blink_led(100,200,((freq >> 3) + 1));      // band is bits 3-5
+  delay(1000);                               // delay
+  blink_led(100,200,((freq & 0x07) + 1));    // channel is bits 0-2
+  delay(750);                                // delay
+  //blink_led(750,0,1);                        // long blink
+}
+
+
+/*
+ * display predefined LED error sequence - 16 fast blinks
+ */
+void flashError(void)
+{
+  blink_led(20, 30, 16);  // LED error sequence
+}
+
+
+/*
+ *  update the frequency registers on TX5823
+ */
 void set_5823_freq(uint8_t freq)
 {
   uint8_t i;
-  uint32_t channelData, rDividerReg;
+  uint32_t channelData;
   
-  // from datasheet, set R-divider to 400d
-  rDividerReg=0x190;
-  
-  if ((((freq >> 3) & 0x07) + 1) > NUM_BANDS) {
-    blink_led(25, 25, 16);  // ERROR
-    return;
+  // if the band value is illegal, flash error sequence only
+  if (freq >= (NUM_BANDS*8)) {
+    flashError();  // flash LED error sequence
+  } else {
+    // read in the channel information from the table, and add 0x00 04 00 00 to it
+    channelData = (pgm_read_word_near(channelTable + freq)) + 0x00040000;
+    
+    // Do we actually need to set register 0x00??
+    //spi_write(0x00, rCounter);      // write R counter-divider (fixed value)
+    spi_write(0x01, channelData);   // write N and A counter-dividers (from channel table)
   }
+}
 
-  // read in the channel information from the table, and add 0x00 04 00 00 to it
-  channelData = (pgm_read_word_near(channelTable + freq)) + 0x00040000;
 
-  SERIAL_ENABLE_LOW();
-  SERIAL_ENABLE_HIGH();
-  SERIAL_ENABLE_LOW();
+/*
+ * execute a complete SPI write operation
+ */
+void spi_write(uint8_t addr, uint32_t data)
+{
+  uint8_t i;
+  uint32_t output;
   
-  SERIAL_SENDBIT0();  // addr = 0x00
-  SERIAL_SENDBIT0();
-  SERIAL_SENDBIT0();
-  SERIAL_SENDBIT0();
+  //              FIRST  -  -  -  -  -  -  -  -  -  -  -  LAST
+  // Data Format: A0  A1  A2  A3  R/W  D0  D1  D2 ... D18  D19
+  output = ((data << 5) | 0x10) + (addr & 0x0f);
   
-  SERIAL_SENDBIT1();  // Write
-  
-  for (i = 20; i > 0; i--) {
-    if (rDividerReg & 0x01) {
-      SERIAL_SENDBIT1();
-    } else {
-      SERIAL_SENDBIT0();
-    }
-    rDividerReg >>= 1;
-  }
-  
-  SERIAL_ENABLE_HIGH();
-  SERIAL_ENABLE_LOW();
-  
-  SERIAL_SENDBIT1();  // addr = 0x01
-  SERIAL_SENDBIT0();
-  SERIAL_SENDBIT0();
-  SERIAL_SENDBIT0();
-  
-  SERIAL_SENDBIT1();  // Write
-  
-  for (i = 20; i > 0; i--) {
-    if (channelData & 0x01) {
-      SERIAL_SENDBIT1();
-    } else {
-      SERIAL_SENDBIT0();
-    }
-    channelData >>= 1;
-  }
-
-  SERIAL_ENABLE_HIGH();
-  
+  // start SPI transaction
+  digitalWrite(SPI_CLOCK_PIN, LOW);
   digitalWrite(SPI_ENABLE_PIN, LOW);
-  digitalWrite(SPI_CLOCK_PIN, LOW);
-  digitalWrite(SPI_DATA_PIN, LOW);
-}
 
-
-void SERIAL_SENDBIT1()
-{
-  digitalWrite(SPI_CLOCK_PIN, LOW);
-  delayMicroseconds(1);
-
-  digitalWrite(SPI_DATA_PIN, HIGH);
-  delayMicroseconds(1);
-  digitalWrite(SPI_CLOCK_PIN, HIGH);
-  delayMicroseconds(1);
-
-  digitalWrite(SPI_CLOCK_PIN, LOW);
-  delayMicroseconds(1);
-}
-
-
-void SERIAL_SENDBIT0()
-{
-  digitalWrite(SPI_CLOCK_PIN, LOW);
-  delayMicroseconds(1);
-
-  digitalWrite(SPI_DATA_PIN, LOW);
-  delayMicroseconds(1);
-  digitalWrite(SPI_CLOCK_PIN, HIGH);
-  delayMicroseconds(1);
-
-  digitalWrite(SPI_CLOCK_PIN, LOW);
-  delayMicroseconds(1);
-}
-
-
-void SERIAL_ENABLE_LOW()
-{
-  delayMicroseconds(1);
-  digitalWrite(SPI_ENABLE_PIN, LOW);
-  delayMicroseconds(1);
-}
-
-
-void SERIAL_ENABLE_HIGH()
-{
-  delayMicroseconds(1);
+  // clock out data LSB first
+  for (i = 25; i > 0; i--) {
+    if (output & 0x01) {
+      digitalWrite(SPI_DATA_PIN, HIGH);  // output 1
+    } else {
+      digitalWrite(SPI_DATA_PIN, LOW);   // output 0
+    }
+    digitalWrite(SPI_CLOCK_PIN, HIGH);   // data latched by 5823 here
+    digitalWrite(SPI_CLOCK_PIN, LOW);    // clock ends low
+    output >>= 1;                        // shift output data
+  }
+  
+  // terminate SPI transaction and set data low
   digitalWrite(SPI_ENABLE_PIN, HIGH);
-  delayMicroseconds(1);
+  digitalWrite(SPI_DATA_PIN, LOW);
 }
